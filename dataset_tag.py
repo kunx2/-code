afe54f06b334f3855e6730920b1465d26cdbaca1
kunx2 <72076184+kunx2@users.noreply.github.com>
2020-10-22 18:54:45 +0800

    Update dataset_tag.py

--- dataset_tag.py
+++ dataset_tag.py
@@ -3,7 +3,7 @@ import cv2
 import time
 sort = 0    #data type
 count = 0
-for i in glob.glob("cap/*.jpg"):										#path to source and get all .jpg file name
+for i in glob.glob("cap/*.jpg"):						#path to source and get all .jpg file name
 	k = cv2.waitKey(1)
 	img = cv2.imread(i)
 	cv2.imwrite("training_data/"+str(sort)+"_"+str(count)+".jpg",img)	#path to destination and rename as tag
