b062acc370d785dccf6893ed6cbf84df08091bc9
kunx2 <72076184+kunx2@users.noreply.github.com>
2020-10-22 18:55:25 +0800

    Update image_capture_for_dataset.py

--- image_capture_for_dataset.py
+++ image_capture_for_dataset.py
@@ -13,29 +13,29 @@ while True:
 	cv2.imshow("read",frame)
 	k = cv2.waitKey(5)
 	time.sleep(0.01)
-	if k == ord('q'):  #press "q" to quit
+	if k == ord('q'):  	#press "q" to quit
 		break
-	elif k == ord('f'):  #press "f" to capture image
+	elif k == ord('f'): 	 #press "f" to capture image
 		_, frame = cap.read()
 		draw = frame.copy()
 		gray = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
 		#blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 		canny = cv2.Canny(gray, 120,200)
 
-		contours,_ = cv2.findContours(canny, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) #"_,contours,_" if error occcur
+		contours,_ = cv2.findContours(canny, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 	#"_,contours,_" if error occcur
 		for contour in contours:
 			area = cv2.contourArea(contour)
-			if area > 100:															#filter out the undesired object(small object) 
+			if area > 100:								#filter out the undesired object(small object) 
 				x,y,w,h = cv2.boundingRect(contour)
 				if abs(x-px)<10 and abs(y-py)<10 and abs(w-pw)<10 and abs(h-ph)<10: #filter out the undesired object(same object)
 					pass
 				else:
 					pre = area
-					cut_img = frame[y:y+h, x:x+w]			  			#cut detected object
-					resize_img = cv2.resize(cut_img,(28,28))  			#resize image to 28*28
+					cut_img = frame[y:y+h, x:x+w]			  	#cut detected object
+					resize_img = cv2.resize(cut_img,(28,28))  		#resize image to 28*28
 					cv2.imwrite("cap/"+str(count)+".jpg", resize_img)	#save the image ( cv2.imwrite( "path to your file" +str(count)+ ".jpg", resize_img)  )
 					cv2.rectangle(draw, (x,y),(x+w,y+h),(0,255,0),1)
-					cv2.imshow("capture",draw)							#show the detected object in another window
+					cv2.imshow("capture",draw)				#show the detected object in another window
 					px,py,pw,ph = x,y,w,h
 					count += 1
 		print(count)
